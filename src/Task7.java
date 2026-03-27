a788a0a30aa641531765d2ee91c08833e572c522
mkairbaeva664-collab <mkairbaeva664@gmail.com>
2026-03-27 08:49:08 +0500

    Update Task7.java

--- src/Task7.java
+++ src/Task7.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()) {
-            int n = sc.nextInt();
-            int[] arr = new int[n];
-            array(arr, 0, sc);
-            rev(arr, n);
-        }
-    }
 
-    public static void array(int[] arr, int i, Scanner sc) {
-        if (i == arr.length) return;
-        arr[i] = sc.nextInt();
-        array(arr, i + 1, sc);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        rev(arr, n - 1);
     }
 
     public static void rev(int[] arr, int n) {
-        if (n == 0) return;
-        System.out.print(arr[n - 1] + " ");
+        if (n < 0) return;
+        System.out.print(arr[n] + " ");
         rev(arr, n - 1);
     }
 }

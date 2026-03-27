5150d388bc7189dd35b10e5392d1e2e4c5d513d2
mkairbaeva664-collab <mkairbaeva664@gmail.com>
2026-03-27 08:59:41 +0500

    Update Task9.java

--- src/Task9.java
+++ src/Task9.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Task9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if (sc.hasNext()) {
-            System.out.println(count(sc.next()));
-        }
+        System.out.println(count(sc.next()))
     }
 
     public static int count(String s) {
